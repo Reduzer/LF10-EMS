93b44d214daa7052efc902bf4587c6ec6eb3de4e
Fyn <83287196+Reduzer@users.noreply.github.com>
2025-01-21 12:39:32 +0100

    Changes 21/01/2025 | Worked on by: Fyn
    Added the HTML part and typescript that throws some errors despite testing in another project

--- Webside/src/app/app.component.ts
+++ Webside/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListEmployeeViewComponent } from "./components/list-employee-view/list-employee-view.component";
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListEmployeeViewComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
